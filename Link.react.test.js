d10872a05fd6cc37c942a71e35ae5b1d288b5aa4
raegar <jamie.myland@gmail.com>
2019-04-14 22:00:20 +0100

    Add test for Link Component

---
+++ Link.react.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from './Link.react';
+import renderer from 'react-test-renderer';
+
+test('Link changes the class when hovered', () => {
+    const component = renderer.create(
+        <Link page="http://www.ucp.ac.uk">University Centre Peterborough</Link>,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSanpshot();
+
+    // manually trigger the callback
+    tree.props.onMouseEnter();
+    //re-rendering now class should be STATUS.HOVERED
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    
+    // manually trigger the callback
+    tree.props.onMouseLeave();
+    // re-rendering now class hsould be STATUS.NORMAL
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+
+})
