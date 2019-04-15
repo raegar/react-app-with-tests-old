d0725e267735310dc944367bd821f077621f54bd
raegar <jamie.myland@gmail.com>
2019-04-15 01:44:10 +0100

    Added mock for user data response

--- src/__tests__/Users.react.test.js
+++ src/__tests__/Users.react.test.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { create } from "react-test-renderer";
 import Users from "../Users.react";
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe("Users component", () => {
     it("shows a list of users", async () => {
+        const response = {
+            data:[
+                { name: "Jamie Myland" },
+                { name: "James Larner" },
+                { name: "Chris Jakeman" },
+            ]
+        };
+        axios.get.mockResolvedValue(response);
         const component = create(<Users />);
         const instance = component.getInstance();
         await instance.componentDidMount();
