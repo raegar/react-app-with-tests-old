e83046de029da686f61d095a430470e4d9023716
raegar <jamie.myland@gmail.com>
2019-04-15 01:55:15 +0100

    Completed test case expecting a list of users

--- src/__tests__/Users.react.test.js
+++ src/__tests__/Users.react.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { create } from "react-test-renderer";
 import Users from "../Users.react";
 import axios from 'axios';
+import { list } from "postcss";
 
 jest.mock('axios');
 
@@ -18,6 +19,12 @@ describe("Users component", () => {
         const component = create(<Users />);
         const instance = component.getInstance();
         await instance.componentDidMount();
-        console.log(instance.state);
+        //console.log(instance.state);
+        const root = component.root;
+        const listOfLi = root.findAll(element => element.type === "li");
+        expect(listOfLi[0].props.children).toBe("Jamie Myland");
+        expect(listOfLi[1].props.children).toBe("James Larner");
+        expect(listOfLi[2].props.children).toBe("Chris Jakeman");
+        
     });
 });
