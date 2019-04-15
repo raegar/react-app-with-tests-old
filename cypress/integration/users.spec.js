a0f0eccb945b2a639320c844ff34fa244f06694e
raegar <jamie.myland@gmail.com>
2019-04-15 20:46:48 +0100

    Added stub to users.spec.js, passes

--- cypress/integration/users.spec.js
+++ cypress/integration/users.spec.js
@@ -1,5 +1,11 @@
 describe("Users App", () => {
     it("can see a list of people" , () => {
+        cy.server();
+        cy.route({
+          method: "GET",
+          url: "**/users",
+          response: [{ name: "Jamie Myland" }]
+        });
         cy.visit("http://localhost:3000/");
         cy.contains("Jamie Myland");
     });
